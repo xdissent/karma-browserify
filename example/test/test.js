7ee4d0d57a3ac38a81776ffd5210c916e6e9a8f2
Greg Thornton <xdissent@me.com>
2013-06-10 19:40:51 -0500

    Fix example test

--- example/test/test.js
+++ example/test/test.js
@@ -4,19 +4,19 @@ var deep = require('../lib/deep');
 var npm = require('methods');
 
 describe('this example', function() {
-  it('should import a helper module', function(){ 
+  it('should import a helper module', function() { 
     expect(helper).toEqual('helper.js');
   });
 
-  it('should import an external module', function(){ 
+  it('should import an external module', function() { 
     expect(external).toEqual('external.js');
   });
 
-  it('should import an external module which imports a module', function(){ 
+  it('should import an external module which imports a module', function() { 
     expect(deep.another).toEqual('another.js');
   });
 
-  it('should import an npm module', function(){ 
-    expect(npm['propfind']).not.toBe(null);
+  it('should import an npm module', function() { 
+    expect(npm).toContain('propfind');
   });
 });
